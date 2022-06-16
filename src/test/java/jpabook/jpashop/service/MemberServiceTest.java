629a838d28c70a6f8b314a86a838cc656e002736
박성우 <swpark@hivelab.co.kr>
2022-06-16 16:32:02 +0900

    [수정] application.yml 수정 및 테스트 클래스 작성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setMemberName("park");
+
+        Long memberIdx = memberService.insertMember(member);
+
+        assertEquals(member, memberRepository.selectMember(memberIdx));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setMemberName("park");
+
+        Member member2 = new Member();
+        member2.setMemberName("park");
+
+        memberService.insertMember(member1);
+        memberService.insertMember(member2);
+    }
+}
